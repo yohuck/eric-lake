ef59b215bd0005f4d81059f38c1ff7fe4a30b956
Eric Lake <ericallenlake@gmail.com>
2022-09-04 14:53:34 -0600

    Adds typewriter effect

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -1,5 +1,27 @@
 <script>
-    // import pic from '../assets/eric.png'
+    let ui = '';
+    let uiChoices = ['b','bo','bol','bold', 'bold','bold', 'bold', 'i', 'in', 'inv', 'invi', 'invit', 'inviti', 'invitin', 'inviting', 'inviting', 'inviting', 'inviting', 'a', 'ac','acc','acce','acces','access','accessi','accessib','accessibl','accessible', 'accessible','accessible', 'accessible','w','wh','whi','whim','whims','whimsi','whimsic','whimsica','whimsical', 'whimsical','whimsical', 'whimsical','e','ef','eff','effe','effec','effect','effecti','effectiv','effective', 'effective','effective', 'effective']
+    let customer = '';
+    let customerChoices = ['c','cu','cus','cust','custo','custom','custome','customer','customer','customer','customer','customer','customer', 'u', 'us','use','user','user','user','user','user','c','cl','cli','clie','clien','client','client','client','client','client', 'p', 'pa', 'par', 'part', 'partn', 'partne', 'partner', 'partner', 'partner', 'partner', 'partner', 'partner']
+    let results = [ui, customer]
+    let choices = [uiChoices, customerChoices]
+
+    const ticker = (index) => {
+        let choice = 0
+        results[index] = choices[index][choice]
+        setInterval(() => {
+            choice++
+            if (choice > choices[index].length - 1){
+                choice = 0;
+            }
+           results[index] = choices[index][choice]
+   
+        }, 200);
+    }
+
+    for (let i = 0; i < results.length; i++){
+        ticker(i)
+    }
 </script>
 
 <section>
@@ -7,11 +29,10 @@
         <h1 class="welcome-hero">
           Howdy, friend.<span class="second"> <br> I'm Eric</span>.
         </h1>
-        <p class="welcome-text">I'm a front-end developer who loves building <span class="ayo">bold</span> UI. 
-           I spent the first decade of my career building <span class="ayo">customer processes</span> and have become an expert in the people on the other side of the screen.
-           I spent the last <span class="ayo">9 months</span> learning to build the web. </p>
+        <p class="welcome-text">I'm a front-end developer who loves building <span class="ayo">{results[0]}</span> UI. 
+           I spent the first decade of my career building <span class="ayo1">customer</span> processes and have become an expert in the people on the other side of the screen. </p>
         
-           <button class="primary">View my resume.</button>
+           <a target="_blank" href="https://docs.google.com/document/d/1NHRPvw4y2NBZ1Rd52Dm9ek9CZjjWjDPKq7od_7C7JN4/edit?usp=sharing"><button class="primary">View my resume.</button></a>
     </div>
 
     <div class="circle-3">
@@ -42,7 +63,7 @@
 
     h1{
  
-         color: var(--teal-5);
+         color: var(--grape-8);
          line-height: var(--font-lineheight-1);
   font-size: var(--font-size-8);
   font-weight: var(--font-weight-9);
@@ -73,7 +94,6 @@
         background-position-x:0px;
         background-image: var(--gradient-2);
         background-size: 2000px 2000px;
-
         border-radius: 999px;
         z-index: 7;
         opacity: 1;
@@ -131,10 +151,27 @@
     } */
 
     .ayo{
+  
+        animation-duration: 5s;
+
+        text-align: center;
+        display: inline-block;
+        width: 9ch;
         /* background-color: var(--gray-9); */
-        color: var(--teal-5);
+        color: var(--grape-8);
         font-weight: var(--font-weight-9);
     }
+    .ayo1{
+  
+  animation-duration: 5s;
+
+  text-align: center;
+  display: inline-block;
+  width: 8ch;
+  /* background-color: var(--gray-9); */
+  color: var(--grape-8);
+  font-weight: var(--font-weight-9);
+}
 
     section{
         /* max-width: 900px; */
@@ -167,15 +204,15 @@
 }
 
 button.primary {
-  background: var(--teal-5);
+  background: var(--indigo-8);
   font-weight: var(--font-weight-7);
   text-shadow: 0 1px 0 var(--red-2);
   color: white;
 }
 
 button.primary:hover {
-  background: var(--teal-4);
-  color: var(--indigo-7);
+  background: var(--grape-8);
+  color: var(--indigo-1);
   text-shadow: none;
 }
 
